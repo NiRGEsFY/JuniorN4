ee72aca09af424068d60d78c6ab8a31f5b28f92c
Nick Rudakov <porol2122@gmail.com>
2022-06-28 23:57:27 +0300

    TestGerHerДобавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] humanParavetrs = new string[4];
+            string[] ask = new string[] { "Как вас зовут?", "Сколько вам лет?", "Где вы работаете?", "Какое у вас образование?" };
+            
+            for (int i = 0; i <= (humanParavetrs.Length - 1); i++)
+            {
+                Console.WriteLine(ask[i]);
+                humanParavetrs[i] = Console.ReadLine();
+            };
+
+            Console.WriteLine("Имя - " + "\t\t" + humanParavetrs[0]);
+            Console.WriteLine("Возраст - " + "\t" + humanParavetrs[1]);
+            Console.WriteLine("Работа - " + "\t" + humanParavetrs[2]);
+            Console.WriteLine("Образование - " + "\t" + humanParavetrs[3]);
+            Console.ReadLine();
+        }
+    }
+}
